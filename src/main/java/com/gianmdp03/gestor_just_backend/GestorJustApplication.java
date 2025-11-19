a5ce464a2c8d01918052a4b18c9d6aa3ef1cc2c4
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-19 16:32:46 -0300

    First commit

---
+++ src/main/java/com/gianmdp03/gestor_just_backend/GestorJustApplication.java
@@ -0,0 +1,13 @@
+package com.gianmdp03.gestor_just_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorJustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorJustApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gianmdp03/gestor_just_backend/GestorJustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gianmdp03.gestor_just_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorJustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
