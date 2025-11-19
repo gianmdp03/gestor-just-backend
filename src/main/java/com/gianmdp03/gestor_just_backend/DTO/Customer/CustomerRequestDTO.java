0d0aaa8949ee58caa26d2c41b9dceee5866035b3
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-19 17:48:09 -0300

    Update Customer and InventoryItem Request DTOS
    Added @noargsconstructor and @builder to this DTOs

--- src/main/java/com/gianmdp03/gestor_just_backend/DTO/Customer/CustomerRequestDTO.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/DTO/Customer/CustomerRequestDTO.java
@@ -1,13 +1,13 @@
 package com.gianmdp03.gestor_just_backend.DTO.Customer;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CustomerRequestDTO {
     @NotBlank
     private String name;
--- src/main/java/com/gianmdp03/gestor_just_backend/DTO/InventoryItem/InventoryItemRequestDTO.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/DTO/InventoryItem/InventoryItemRequestDTO.java
@@ -2,15 +2,15 @@ package com.gianmdp03.gestor_just_backend.DTO.InventoryItem;
 
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class InventoryItemRequestDTO {
     @NotNull
     private Long productId;
