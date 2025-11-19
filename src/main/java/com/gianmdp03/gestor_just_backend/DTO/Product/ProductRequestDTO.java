e24dd376a330b9fbe6ec578042275441d794f2f8
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-19 17:50:52 -0300

    Add ProductRequestDTO

--- src/main/java/com/gianmdp03/gestor_just_backend/DTO/Product/ProductRequestDTO.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/DTO/Product/ProductRequestDTO.java
@@ -1,4 +1,16 @@
 package com.gianmdp03.gestor_just_backend.DTO.Product;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductRequestDTO {
+    @NotBlank
+    private String name;
+
+    private String imageUrl;
 }
