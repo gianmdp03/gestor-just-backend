d4deed97969f1d632bdf719427695294f2128f46
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-19 17:32:38 -0300

    Add ubication entity

--- src/main/java/com/gianmdp03/gestor_just_backend/Model/Ubication.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Model/Ubication.java
@@ -1,4 +1,32 @@
 package com.gianmdp03.gestor_just_backend.Model;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ubications")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Ubication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "ubication")
+    private List<InventoryItem> inventoryItem = new ArrayList<>();
+
+    @Builder
+    public Ubication(String name) {
+        this.name = name;
+    }
 }
