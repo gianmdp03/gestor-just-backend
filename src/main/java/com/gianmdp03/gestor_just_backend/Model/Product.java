65626f77e36106b7eb25cb528a4e5088366b922f
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-19 17:22:10 -0300

    Add Product entity

--- src/main/java/com/gianmdp03/gestor_just_backend/Model/Product.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Model/Product.java
@@ -1,4 +1,35 @@
 package com.gianmdp03.gestor_just_backend.Model;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String imageUrl;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Order> orders = new ArrayList<>();
+
+    @Builder
+    public Product(String name, String imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
 }
