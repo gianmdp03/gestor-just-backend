22d31650bf8503f2b429c26015a41ca8f1d57807
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-19 17:10:26 -0300

    Add inventoryItem entity

--- src/main/java/com/gianmdp03/gestor_just_backend/Model/InventoryItem.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Model/InventoryItem.java
@@ -1,4 +1,42 @@
 package com.gianmdp03.gestor_just_backend.Model;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "inventory_items")
+@Getter
+@Setter
+@NoArgsConstructor
 public class InventoryItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "ubication_id")
+    private Ubication ubication;
+
+    @Column(nullable = false)
+    private int stock;
+
+    @Column(nullable = false)
+    private LocalDate expireDate;
+
+    @Builder
+    public InventoryItem(Product product, Ubication ubication, int stock, LocalDate expireDate) {
+        this.product = product;
+        this.ubication = ubication;
+        this.stock = stock;
+        this.expireDate = expireDate;
+    }
 }
