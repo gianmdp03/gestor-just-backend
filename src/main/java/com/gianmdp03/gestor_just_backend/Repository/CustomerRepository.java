3bccf1e769568688bfd87d69b1db87b0ce1c9de8
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-20 14:24:21 -0300

    Add CustomerRepository

--- src/main/java/com/gianmdp03/gestor_just_backend/Repository/CustomerRepository.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Repository/CustomerRepository.java
@@ -1,4 +1,13 @@
 package com.gianmdp03.gestor_just_backend.Repository;
 
-public interface CustomerRepository {
+import com.gianmdp03.gestor_just_backend.Model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByPhoneNumber(String phoneNumber);
+    boolean existsByPhoneNumber(String phoneNumber);
 }
