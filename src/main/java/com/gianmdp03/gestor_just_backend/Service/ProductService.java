3b5ec0eb98b805822e48e91388574545d6e02ae7
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-20 15:07:04 -0300

    Add ProductService

--- src/main/java/com/gianmdp03/gestor_just_backend/Service/ProductService.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Service/ProductService.java
@@ -1,4 +1,13 @@
-package com.gianmdp03.gestor_just_backend.Service.Impl;
+package com.gianmdp03.gestor_just_backend.Service;
+
+import com.gianmdp03.gestor_just_backend.DTO.Product.ProductDetailDTO;
+import com.gianmdp03.gestor_just_backend.DTO.Product.ProductListDTO;
+import com.gianmdp03.gestor_just_backend.DTO.Product.ProductRequestDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProductService {
+    ProductDetailDTO addProduct(ProductRequestDTO productRequestDTO);
+    Page<ProductListDTO> listProducts(Pageable pageable);
+    void deleteProduct(Long id);
 }
