d699e5f47c5970cf3c3557b5af0a8e631c06ea6c
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-20 14:29:03 -0300

    Add UbicationRepository

--- src/main/java/com/gianmdp03/gestor_just_backend/Repository/UbicationRepository.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Repository/UbicationRepository.java
@@ -1,4 +1,9 @@
 package com.gianmdp03.gestor_just_backend.Repository;
 
-public interface UbicationRepository {
+import com.gianmdp03.gestor_just_backend.Model.Ubication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UbicationRepository extends JpaRepository<Ubication, Long>{
 }
