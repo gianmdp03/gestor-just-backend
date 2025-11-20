a6e9511fe3b09e25cf99740cec184d9d2fead098
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-20 14:39:36 -0300

    Update List methods to Page in repositories

--- src/main/java/com/gianmdp03/gestor_just_backend/Repository/InventoryItemRepository.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Repository/InventoryItemRepository.java
@@ -3,17 +3,17 @@ package com.gianmdp03.gestor_just_backend.Repository;
 import com.gianmdp03.gestor_just_backend.Model.InventoryItem;
 import com.gianmdp03.gestor_just_backend.Model.Product;
 import com.gianmdp03.gestor_just_backend.Model.Ubication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Repository
 public interface InventoryItemRepository extends JpaRepository<InventoryItem, Long> {
-    List<InventoryItem> findAllByProduct(Product product);
-    List<InventoryItem> findAllByUbication(Ubication ubication);
-    List<InventoryItem> findAllByOrderByExpireDateAsc();
-    List<InventoryItem> findByExpireDateBefore(LocalDate date);
-    List<InventoryItem> findByExpireDateGreaterThanEqual(LocalDate date);
+    Page<InventoryItem> findAllByProduct(Product product, Pageable pageable);
+    Page<InventoryItem> findAllByUbication(Ubication ubication, Pageable pageable);
+    Page<InventoryItem> findByExpireDateBefore(LocalDate date, Pageable pageable);
+    Page<InventoryItem> findByExpireDateGreaterThanEqual(LocalDate date, Pageable pageable);
 }
--- src/main/java/com/gianmdp03/gestor_just_backend/Repository/OrderRepository.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Repository/OrderRepository.java
@@ -1,13 +1,14 @@
 package com.gianmdp03.gestor_just_backend.Repository;
 
 import com.gianmdp03.gestor_just_backend.Model.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findBySaleDateBetween(LocalDate startDate, LocalDate endDate);
+    Page<Order> findBySaleDateBetween(LocalDate startDate, LocalDate endDate, Pageable pageable);
 }
