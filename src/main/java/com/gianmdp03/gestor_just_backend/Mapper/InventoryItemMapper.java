71f41af1f73cd88fba1ae6c0dbfd002d34046516
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-20 15:56:16 -0300

    Add InventoryItemMapper

--- src/main/java/com/gianmdp03/gestor_just_backend/Mapper/InventoryItemMapper.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Mapper/InventoryItemMapper.java
@@ -1,4 +1,19 @@
 package com.gianmdp03.gestor_just_backend.Mapper;
 
+import com.gianmdp03.gestor_just_backend.DTO.InventoryItem.InventoryItemListDTO;
+import com.gianmdp03.gestor_just_backend.DTO.InventoryItem.InventoryItemRequestDTO;
+import com.gianmdp03.gestor_just_backend.Model.InventoryItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface InventoryItemMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "product", ignore = true)
+    @Mapping(target = "ubication", ignore = true)
+    InventoryItem toEntity(InventoryItemRequestDTO dto);
+    InventoryItemListDTO toDto(InventoryItem entity);
+    List<InventoryItemListDTO> toListDto(List<InventoryItem> entities);
 }
