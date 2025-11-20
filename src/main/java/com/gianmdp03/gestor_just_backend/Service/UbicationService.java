5b951db5bf42cc6326c12730311026f8e244904f
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-20 15:08:37 -0300

    Add UbicationService

--- src/main/java/com/gianmdp03/gestor_just_backend/Service/UbicationService.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Service/UbicationService.java
@@ -1,4 +1,13 @@
-package com.gianmdp03.gestor_just_backend.Service.Impl;
+package com.gianmdp03.gestor_just_backend.Service;
+
+import com.gianmdp03.gestor_just_backend.DTO.Ubication.UbicationDetailDTO;
+import com.gianmdp03.gestor_just_backend.DTO.Ubication.UbicationListDTO;
+import com.gianmdp03.gestor_just_backend.DTO.Ubication.UbicationRequestDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface UbicationService {
+    UbicationDetailDTO addUbication(UbicationRequestDTO ubicationRequestDTO);
+    Page<UbicationListDTO> listUbications(Pageable pageable);
+    void deleteUbication(Long id);
 }
