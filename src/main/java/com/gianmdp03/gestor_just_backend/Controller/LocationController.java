a12587c5a17ed69e03e63fd7a225cbc8881b4866
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-23 11:57:56 -0300

    Add LocationController

--- src/main/java/com/gianmdp03/gestor_just_backend/Controller/LocationController.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/Controller/LocationController.java
@@ -1,13 +1,39 @@
 package com.gianmdp03.gestor_just_backend.controller;
 
+import com.gianmdp03.gestor_just_backend.dto.location.LocationDetailDTO;
+import com.gianmdp03.gestor_just_backend.dto.location.LocationListDTO;
+import com.gianmdp03.gestor_just_backend.dto.location.LocationRequestDTO;
 import com.gianmdp03.gestor_just_backend.service.LocationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/locations")
 @RequiredArgsConstructor
 public class LocationController {
     private final LocationService locationService;
+
+    @PostMapping
+    public ResponseEntity<LocationDetailDTO> addLocation(@Valid @RequestBody LocationRequestDTO locationRequestDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(locationService.addLocation(locationRequestDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<LocationListDTO>> listLocations(
+            @PageableDefault(page = 0, size = 10, sort = "name", direction = Sort.Direction.DESC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(locationService.listLocations(pageable));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLocation(@PathVariable Long id){
+        locationService.deleteLocation(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
