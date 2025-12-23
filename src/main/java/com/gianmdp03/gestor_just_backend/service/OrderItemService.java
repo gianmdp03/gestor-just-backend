0d4924f67af54f85b1eab1bca8875ecac94c1450
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-12-23 11:33:40 -0300

    Made service + addOrderItem to ServiceImpl

--- src/main/java/com/gianmdp03/gestor_just_backend/service/OrderItemService.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/service/OrderItemService.java
@@ -1,4 +1,9 @@
 package com.gianmdp03.gestor_just_backend.service;
 
+import com.gianmdp03.gestor_just_backend.dto.orderitem.OrderItemListDTO;
+import com.gianmdp03.gestor_just_backend.dto.orderitem.OrderItemRequestDTO;
+
 public interface OrderItemService {
+    OrderItemListDTO addOrderItem(OrderItemRequestDTO dto);
+    OrderItemListDTO listOrderItems(Long orderId);
 }
--- src/main/java/com/gianmdp03/gestor_just_backend/service/impl/OrderItemServiceImpl.java
+++ src/main/java/com/gianmdp03/gestor_just_backend/service/impl/OrderItemServiceImpl.java
@@ -1,4 +1,46 @@
 package com.gianmdp03.gestor_just_backend.service.impl;
 
-public class OrderItemServiceImpl {
+import com.gianmdp03.gestor_just_backend.dto.orderitem.OrderItemListDTO;
+import com.gianmdp03.gestor_just_backend.dto.orderitem.OrderItemRequestDTO;
+import com.gianmdp03.gestor_just_backend.exception.NotFoundException;
+import com.gianmdp03.gestor_just_backend.mapper.OrderItemMapper;
+import com.gianmdp03.gestor_just_backend.model.Order;
+import com.gianmdp03.gestor_just_backend.model.OrderItem;
+import com.gianmdp03.gestor_just_backend.model.Product;
+import com.gianmdp03.gestor_just_backend.repository.OrderItemRepository;
+import com.gianmdp03.gestor_just_backend.repository.OrderRepository;
+import com.gianmdp03.gestor_just_backend.repository.ProductRepository;
+import com.gianmdp03.gestor_just_backend.service.OrderItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderItemServiceImpl implements OrderItemService {
+    private final OrderItemRepository orderItemRepository;
+    private final OrderItemMapper orderItemMapper;
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+
+    @Override
+    @Transactional
+    public OrderItemListDTO addOrderItem(OrderItemRequestDTO dto) {
+        Order order = orderRepository.findById(dto.orderId())
+                .orElseThrow(() -> new NotFoundException("Order id does not exist"));
+        Product product = productRepository.findById(dto.productId())
+                .orElseThrow(() -> new NotFoundException("Product id does not exist"));
+        OrderItem orderItem = orderItemMapper.toEntity(dto);
+        orderItem.setOrder(order);
+        orderItem.setProduct(product);
+        orderItem = orderItemRepository.save(orderItem);
+        
+        return orderItemMapper.toListDto(orderItem);
+    }
+
+    @Override
+    public OrderItemListDTO listOrderItems(Long orderId) {
+        return null;
+    }
 }
